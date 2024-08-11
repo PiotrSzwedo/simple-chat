9faecf1aebff4f49e96d86a2e47e88a4552669c6
me <>
2024-08-11 20:37:33 +0200

    Added router

---
+++ controller/Controller.php
@@ -0,0 +1,36 @@
+<?php 
+
+abstract class Controller{
+    public function __construct($action, $parameters){
+        if(empty($action)){
+            $this->default();
+        }else{
+
+            if (method_exists($this, $action)){
+                $reflectionMethod = new ReflectionMethod(get_class($this), $action);
+            }
+
+            $requiredParams = $reflectionMethod->getNumberOfRequiredParameters();
+            $params = $reflectionMethod->getNumberOfParameters();
+
+            if (
+                    $reflectionMethod->isPublic() && 
+                    count($parameters) >= $requiredParams &&
+                    count($parameters) <= $params
+                ) {
+                $this->$action(...$parameters);
+            }
+            else{
+                $this->error404();
+            }
+        }
+    }
+
+    public function default(){
+
+    }
+
+    private function error404(){
+
+    }
+}
---
+++ router/Router.php
@@ -0,0 +1,33 @@
+<?php 
+
+class Router{
+    protected $controller;
+    protected $action;
+    protected $parameters;
+
+    public function __construct(){
+
+    }
+
+    public function praseUri(){
+        $parsedUrl = explode('/', $_SERVER["REQUEST_URI"]);
+
+        array_shift($parsedUrl);
+
+        $this->controller = array_shift($parsedUrl);
+        $this->action = str_replace("-", "_", array_shift($parsedUrl));
+        $this->parameters = str_replace("-", "_", $parsedUrl);
+    }
+
+    public function loadModule()
+    {
+        $this->action = str_replace("-", "_", $this->action);
+
+        $controller = $this->controller;
+
+        if (class_exists(ucfirst($controller), true) || class_exists($controller, true) || class_exists(strtolower($controller), true))
+            $this->controller = new $controller($this->action, $this->parameters);
+        else
+            $this->controller = new Home($this->action, $this->parameters);
+    }
+}
